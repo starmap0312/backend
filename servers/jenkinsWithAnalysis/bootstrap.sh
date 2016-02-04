f5e9885fa6131913557cffb67b77366541434057
starmap0312 <starmap0312@gmail.com>
2016-02-04 15:33:54 +0800

    add jenkins

---
+++ servers/jenkinsWithAnalysis/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Jenkins: Jobs
+mkdir -p /data/jenkins/jobs/myFirstJob
+cp /vagrant/myFirstJob/config.xml /data/jenkins/jobs/myFirstJob/.
+
+# Jenkins: Dependency plugins
+mkdir -p /data/jenkins/plugins
+cd /data/jenkins/plugins
+wget https://updates.jenkins-ci.org/latest/credentials.hpi
+wget https://updates.jenkins-ci.org/latest/ssh-credentials.hpi
+wget https://updates.jenkins-ci.org/latest/promoted-builds.hpi
+wget https://updates.jenkins-ci.org/latest/javadoc.hpi
+wget https://updates.jenkins-ci.org/latest/mailer.hpi
+wget https://updates.jenkins-ci.org/latest/token-macro.hpi
+wget https://updates.jenkins-ci.org/latest/maven-plugin.hpi
+wget https://updates.jenkins-ci.org/latest/scm-api.hpi
+wget https://updates.jenkins-ci.org/latest/subversion.hpi
+wget https://updates.jenkins-ci.org/latest/run-condition.hpi
+wget https://updates.jenkins-ci.org/latest/conditional-buildstep.hpi
+wget https://updates.jenkins-ci.org/latest/parameterized-trigger.hpi
+wget https://updates.jenkins-ci.org/latest/git-client.hpi
+wget https://updates.jenkins-ci.org/latest/dashboard-view.hpi
+wget https://updates.jenkins-ci.org/latest/ant.hpi
+wget https://updates.jenkins-ci.org/latest/analysis-core.hpi
+
+# Jenkins: Plugins
+wget https://updates.jenkins-ci.org/latest/git.hpi
+wget https://updates.jenkins-ci.org/latest/gradle.hpi
+wget https://updates.jenkins-ci.org/latest/analysis-collector.hpi
+wget https://updates.jenkins-ci.org/latest/checkstyle.hpi
+wget https://updates.jenkins-ci.org/latest/findbugs.hpi
+wget https://updates.jenkins-ci.org/latest/pmd.hpi
+
+echo "docker run -d -p 0.0.0.0:8080:8080 -v /data/jenkins:/jenkins starmap0312/cd_jenkins_with_analysis" >/opt/cd_jenkins_with_analysis.sh
+chmod 744 /opt/cd_jenkins_with_analysis.sh
